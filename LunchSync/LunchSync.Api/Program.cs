b98ce6d38ee3831d5d49039287c63498d130b3c7
markByOverflow <mark.lev1369@gmail.com>
2026-03-16 16:53:05 +0700

    chore(api): setup swagger

--- LunchSync/LunchSync.Api/Program.cs
+++ LunchSync/LunchSync.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 
 namespace LunchSync.Api
 {
@@ -10,7 +11,15 @@ namespace LunchSync.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "LunchSync API",
+                    Version = "v1"
+                });
+            });
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
@@ -18,13 +27,17 @@ namespace LunchSync.Api
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "LunchSync API v1");
+                    c.RoutePrefix = string.Empty; // serve UI at application root
+                });
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
