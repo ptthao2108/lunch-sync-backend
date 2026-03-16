4fbdd5ed7d8db3449fde84dc98a9a0cb6ac175d6
markByOverflow <mark.lev1369@gmail.com>
2026-03-16 17:03:18 +0700

    feat: init DI in Core and Infrastructure

---
+++ LunchSync/LunchSync.Core/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchSync.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
