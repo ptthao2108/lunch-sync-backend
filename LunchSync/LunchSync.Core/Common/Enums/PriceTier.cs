7a9c51c67a71b9a843ff95fb857e13156af924bf
markByOverflow <mark.lev1369@gmail.com>
2026-03-16 16:15:12 +0700

    feat(core): add enums for status

---
+++ LunchSync/LunchSync.Core/Common/Enums/PriceTier.cs
@@ -0,0 +1,10 @@
+﻿namespace LunchSync.Core.Common.Enums
+{
+    public enum PriceTier
+    {
+        Under40k = 0,      
+        From40To70k = 1,   
+        From70To120k = 2,  
+        Over120k = 3       
+    }
+}
