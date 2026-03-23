f489104abad55b40ccd23a09d38dc80362208680
Phan Thanh Thao <phanthanhthao.21082005@gmail.com>
2026-03-23 09:40:29 +0700

    feat(Sessions): new IPinManager

---
+++ LunchSync/LunchSync.Core/Modules/Sessions/IPinManager.cs
@@ -0,0 +1,10 @@
+﻿namespace LunchSync.Core.Modules.Sessions;
+
+public interface IPinManager
+{
+    // Lấy PIN từ Pool hoặc Gen mới nếu Pool trống
+    Task<string> GetUnusedPinAsync();
+
+    // Đưa PIN trở lại Pool khi Session kết thúc
+    Task ReleasePinAsync(string pin);
+}
