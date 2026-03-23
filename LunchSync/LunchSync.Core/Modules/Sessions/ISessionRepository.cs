c0c8cadc234220af09c88d9c636e0448c2b3003c
Phan Thanh Thao <phanthanhthao.21082005@gmail.com>
2026-03-23 23:13:18 +0700

    feat(SessionRepository): implement interface

--- LunchSync/LunchSync.Core/Modules/Sessions/ISessionRepository.cs
+++ LunchSync/LunchSync.Core/Modules/Sessions/ISessionRepository.cs
@@ -1,11 +1,10 @@
 ﻿using LunchSync.Core.Modules.Sessions.Entities;
 using LunchSync.Core.Common.Enums;
 namespace LunchSync.Core.Modules.Sessions;
+
 public interface ISessionRepository
 {
-    Task<Guid> SaveHistoryAsync(Session Session);
-    Task<Session?> GetHistoryByIdAsync(Guid SessionId);
-    Task<List<Session>> GetSessionsToExpireAsync(DateTime threshold);
-    Task UpdateSessionsStatusAsync(List<Guid> SessionIds, SessionStatus status);
+    Task<Guid> SaveHistoryAsync(Session session);
+    Task<Session?> GetHistoryByIdAsync(Guid sessionId);
     Task<Session?> GetLastSessionByHostIdAsync(Guid hostId);
 }
---
+++ LunchSync/LunchSync.Infrastructure/Persistence/Repositories/SessionRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using LunchSync.Core.Common.Enums;
+using LunchSync.Core.Modules.Sessions.Entities;
+using LunchSync.Core.Modules.Sessions;
+
+namespace LunchSync.Infrastructure.Persistence.Repositories;
+
+public class SessionRepository : ISessionRepository
+{
+    private readonly AppDbContext _context;
+
+    public SessionRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Session?> GetHistoryByIdAsync(Guid sessionId)
+    {
+        return await _context.Sessions
+            .AsNoTracking()
+            .Include(s => s.Participants) // Load danh sách người chơi
+            .FirstOrDefaultAsync(s => s.Id == sessionId);
+    }
+    public async Task<Session?> GetLastSessionByHostIdAsync(Guid hostId)
+    {
+        return await _context.Sessions
+            .AsNoTracking()
+            .Where(s => s.HostId == hostId)
+            .OrderByDescending(s => s.CreatedAt)
+            .FirstOrDefaultAsync();
+    }
+    public async Task<Guid> SaveHistoryAsync(Session session)
+    {
+        _context.Sessions.Add(session);
+        await _context.SaveChangesAsync();
+        return session.Id;
+    }
+}
